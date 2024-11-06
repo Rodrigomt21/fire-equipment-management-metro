df924908f3bd963ac6ec0f81281c2bde03290baf
Rodrigo <23.00068-6@maua.br>
2024-11-06 15:06:54 -0300

    arumando tela cadastro

--- pi_metro/lib/screens/cadastro.dart
+++ pi_metro/lib/screens/cadastro.dart
@@ -10,109 +10,103 @@ class CadastroPage extends StatelessWidget {
     final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          final isSmallScreen = constraints.maxWidth < 600;
+      body: Stack(
+        children: [
+          // Imagem de fundo ajustada ao tamanho da tela
+          Container(
+            width: screenWidth,
+            height: screenHeight,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('lib/imgs/telaCadastro.png'),
+                fit: BoxFit.cover, // Garante que a imagem cubra toda a tela
+              ),
+            ),
+          ),
 
-          return Stack(
-            children: [
-              // Imagem de fundo ajustada ao tamanho da tela
-              Container(
-                width: constraints.maxWidth,
-                height: constraints.maxHeight,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('lib/imgs/telaCadastro.png'),
-                    fit: BoxFit.cover, // Garante que a imagem cubra toda a tela
+          // Campo de Email
+          Positioned(
+            left: 973,
+            top: screenHeight * 0.3,
+            child: SizedBox(
+              width: 298,
+              height: 50,
+              child: _buildTextField('Enter Email'),
+            ),
+          ),
+
+          // Campo de Senha
+          Positioned(
+            left: 973,
+            top: screenHeight * 0.4,
+            child: SizedBox(
+              width: 298,
+              height: 50,
+              child: _buildTextField('Password', isPassword: true),
+            ),
+          ),
+
+          // Campo de Confirmação de Senha
+          Positioned(
+            left: 973,
+            top: screenHeight * 0.5,
+            child: SizedBox(
+              width: 298,
+              height: 50,
+              child: _buildTextField('Confirm Password', isPassword: true),
+            ),
+          ),
+
+          // Botão de Registrar estilizado
+          Positioned(
+            left: 973,
+            top: screenHeight * 0.6,
+            child: SizedBox(
+              width: 298,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF001489),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  'Register',
+                  style: GoogleFonts.poppins(
+                    fontSize: 20,
+                    color: Colors.white,
                   ),
                 ),
               ),
+            ),
+          ),
 
-              // Campo de Email
-              Positioned(
-                left: isSmallScreen ? screenWidth * 0.1 : 973,
-                top: screenHeight * 0.3,
-                child: SizedBox(
-                  width: isSmallScreen ? screenWidth * 0.8 : 298,
-                  height: 50,
-                  child: _buildTextField('Enter Email'),
-                ),
-              ),
-
-              // Campo de Senha
-              Positioned(
-                left: isSmallScreen ? screenWidth * 0.1 : 973,
-                top: screenHeight * 0.4,
-                child: SizedBox(
-                  width: isSmallScreen ? screenWidth * 0.8 : 298,
-                  height: 50,
-                  child: _buildTextField('Password', isPassword: true),
-                ),
-              ),
-
-              // Campo de Confirmação de Senha
-              Positioned(
-                left: isSmallScreen ? screenWidth * 0.1 : 973,
-                top: screenHeight * 0.5,
-                child: SizedBox(
-                  width: isSmallScreen ? screenWidth * 0.8 : 298,
-                  height: 50,
-                  child: _buildTextField('Confirm Password', isPassword: true),
-                ),
-              ),
-
-              // Botão de Registrar estilizado
-              Positioned(
-                left: isSmallScreen ? screenWidth * 0.1 : 973,
-                top: screenHeight * 0.6,
-                child: SizedBox(
-                  width: isSmallScreen ? screenWidth * 0.8 : 298,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF001489),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    child: Text(
-                      'Register',
-                      style: GoogleFonts.poppins(
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),
-                    ),
+          // Botão de login transparente e sem texto
+          Positioned(
+            left: 305,
+            top: 405,
+            child: SizedBox(
+              width: 90,
+              height: 15,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/login');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                  padding: EdgeInsets.zero,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
                 ),
+                child: const SizedBox.shrink(),
               ),
-
-              // Botão de login transparente e sem texto
-              Positioned(
-                left: isSmallScreen ? screenWidth * 0.7 : 315,
-                top: screenHeight * 0.6,
-                child: SizedBox(
-                  width: 100,
-                  height: 15,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/login');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.transparent,
-                      shadowColor: Colors.transparent,
-                      padding: EdgeInsets.zero,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    child: const SizedBox.shrink(),
-                  ),
-                ),
-              ),
-            ],
-          );
-        },
+            ),
+          ),
+        ],
       ),
     );
   }
